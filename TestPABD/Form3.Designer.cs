25e5454ae4c0628624498c3682f5b4ac97a18a57
beatrixdaa <beatrixdevanti21@gmail.com>
2023-07-05 17:29:01 +0700

    add form 3

--- TestPABD/Form3.Designer.cs
+++ TestPABD/Form3.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TestPABD
 {
-    partial class Form2
+    partial class Form3
     {
         /// <summary>
         /// Required designer variable.
@@ -28,32 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(463, 102);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // Form2
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Name = "Form2";
-            this.Text = "Form2";
-            this.ResumeLayout(false);
-
+            this.Text = "Form3";
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
     }
 }
